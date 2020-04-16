e70732e3ab1e88af615e0c974004a088ff75547e
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-04-17 01:51:10 +0200

    Added example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CarTest extends TestCase
+{
+    /**
+     * Example test
+     *
+     * @test
+     * @return void
+     */
+    public function test_can_example()
+    {
+
+    }
+}
