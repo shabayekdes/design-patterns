a10972fc34d89ea2f774cc74e1f96ef8f32276dc
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-04-17 03:16:38 +0200

    Add product example to builder patterns

---
+++ src/Creational/Builder/Product/Interfaces/Builder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Creational\Builder\Product\Interfaces;
+
+/**
+ * The Builder interface specifies methods for creating the different parts of
+ * the Product objects.
+ */
+interface Builder
+{
+    public function producePartA(): void;
+
+    public function producePartB(): void;
+
+    public function producePartC(): void;
+}
